e190008c3375cf2366cfd28e34602d042f498c2c
krystasis <maronsuudao@outlook.com>
2022-11-28 19:49:33 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,7 @@ export async function getStaticProps() {
     }
 }
 
-export default function Home({ allPostsData }: {allPostsData: any}) {
+export default function Home({ allPostsData: any }) {
   return (
     <Layout home>
       <Head>
@@ -51,7 +51,7 @@ export default function Home({ allPostsData }: {allPostsData: any}) {
         <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
           <h2 className={utilStyles.headingLg}>Blog</h2>
           <ul className={utilStyles.list}>
-            {allPostsData.map(({ id, date, title}: any) => (
+            {allPostsData.map(({ id: any, date: any, title: any}) => (
               <li className={utilStyles.listItem} key={id}>
                 <Link href={`/posts/${id}`}>
                     {title}
