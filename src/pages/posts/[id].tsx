2e2a09ec377ac486e1dffa51c6d07b55d19e3851
krystasis <maronsuudao@outlook.com>
2022-11-28 20:53:51 +0900

    test

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -6,7 +6,7 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default function Post({ postData }) {
     return (
-        <Layout post>
+        <Layout null>
             <Head>
                 <title>{postData.title}</title>
             </Head>
