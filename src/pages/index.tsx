964f53030cdc80269165d14143fcc16237146186
krystasis <maronsuudao@outlook.com>
2022-11-28 19:58:31 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -51,7 +51,7 @@ export default function Home({ allPostsData }: {allPostsData: any}) {
         <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
           <h2 className={utilStyles.headingLg}>Blog</h2>
           <ul className={utilStyles.list}>
-            {allPostsData.map(({ id, date, title}, {id: any, date: any, title: any}) => (
+            {allPostsData.map(({ id, date, title}: {id: any, date: any, title: any}) => (
               <li className={utilStyles.listItem} key={id}>
                 <Link href={`/posts/${id}`}>
                     {title}
